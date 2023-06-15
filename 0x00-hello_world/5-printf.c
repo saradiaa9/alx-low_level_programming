08beba49392e34d59f87c82748939ba6aafe6a69
Sara Diaa <saradiaa9@example.com>
2023-06-15 16:00:00 -0700

    s

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ *              followed by a new line using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
