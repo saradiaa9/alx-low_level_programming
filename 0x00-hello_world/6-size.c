87ecffbe88f017a3cb47a1ce04d7b0245e9d707b
Sara Diaa <saradiaa9@example.com>
2023-06-15 16:13:38 -0700

    s

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-/*sizesssssss*/
+/**
+ * main - Entry point
+ *
+ * Description: Prints the size of various types on the computer it is compiled and run on.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("Size of a char: %ld byte(s)\n", sizeof(char));
