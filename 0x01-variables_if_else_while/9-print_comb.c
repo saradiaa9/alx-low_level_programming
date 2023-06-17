15d0286d869dff2e6d625a08a2d15c3250039a15
Sara Diaa <saradiaa9@example.com>
2023-06-17 14:51:41 -0700

    s

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+int comma = 0;  /* flag to indicate whether to print comma and space */
+
+while (i < 100)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+
+comma = (i != 99);  /* set flag to 1 except for last pair */
+
+putchar(comma * ', ');
+
+i++;
+}
+
+putchar('\n');
+
+return (0);
+}
