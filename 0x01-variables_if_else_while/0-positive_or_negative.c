d7a835791ab5d0f21bf5e0cab4ec0a626fcd525a
Sara Diaa <saradiaa9@example.com>
2023-06-17 13:42:01 -0700

    s

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,16 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("%d\n", n);
-if (n > 0) {
+if (n > 0)
+{
 printf("is positive\n");
-} else if (n == 0) {
+}
+else if (n == 0)
+{
 printf("is zero\n");
-} else {
+}
+else
+{
 printf("is negative\n");
 }
 return (0);
