e8b2c31ac5da2d27b9c8c7b83bc3c442a3a6be23
Sara Diaa <saradiaa9@example.com>
2023-06-23 10:06:12 -0700

    s

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
  * print_most_numbers - prints the numbers, from 0 to 9,
- * except for 2 and 4,
- *                      followed by a new line
+ * except for 2 and 4, followed by a new line
  *
  * Return: void
  */
