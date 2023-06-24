067b0908a469211c04a933fb7591f2e046d87652
Sara Diaa <saradiaa9@example.com>
2023-06-23 18:36:46 -0700

    s

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,7 +9,6 @@
 * @year: year
 * Return: void
 */
-
 void print_remaining_days(int month, int day, int year)
 {
 int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -22,6 +21,8 @@ if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 days_in_month[2] = 29;
 }
 
+if (month >= 1 && month <= 12 && day >= 1 && day <= days_in_month[month])
+{
 
 for (i = 1; i < month; i++)
 {
@@ -32,8 +33,6 @@ days += days_in_month[i];
 days += day;
 
 
-if (month <= 12 && day <= days_in_month[month])
-{
 printf("Day of the year: %d\n", days);
 printf("Remaining days: %d\n", 365 + ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? 1 : 0) - days);
 }
