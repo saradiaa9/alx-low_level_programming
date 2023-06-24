aec93457fe7454a93d49535a27d2214745b983e9
Sara Diaa <saradiaa9@example.com>
2023-06-23 18:23:46 -0700

    s

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,22 +16,22 @@ int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 int days = 0;
 int i;
 
-// Check if year is a leap year
+
 if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 {
 days_in_month[2] = 29; // February has 29 days in a leap year
 }
 
-// Calculate the total number of days up until the current month
+
 for (i = 1; i < month; i++)
 {
 days += days_in_month[i];
 }
 
-// Add the current day to the total
+
 days += day;
 
-// Print the remaining days in the year
+
 if (month <= 12 && day <= days_in_month[month])
 {
 printf("Day of the year: %d\n", days);
