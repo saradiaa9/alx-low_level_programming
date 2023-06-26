942a780cec20c3fa0b724f0c1cd211e039e670fa
Sara Diaa <saradiaa9@example.com>
2023-06-26 16:01:11 -0700

    s

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - prints a string to stdout, followed by a new line
+ * @str: pointer to the string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+
+while (*str != '\0')
+{
+putchar(*str);
+str++;
+}
+
+putchar('\n');
+
+}
