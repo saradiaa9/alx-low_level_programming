84f5fb059daafd4be1faebc539f1b9550066efb3
Sara Diaa <saradiaa9@example.com>
2023-06-26 16:22:05 -0700

    s

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: pointer to the string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+int length = 0;
+int i;
+
+
+while (s[length] != '\0')
+{
+length++;
+}
+
+
+for (i= 0; i < length / 2; i++)
+{
+char temp = s[i];
+s[i] = s[length - i - 1];
+s[length - i - 1] = temp;
+}
+
+}
