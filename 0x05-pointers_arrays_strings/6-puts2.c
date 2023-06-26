ad7e83147fb14b53e0fe85c2c92d8d27f182fb83
Sara Diaa <saradiaa9@example.com>
2023-06-26 16:51:42 -0700

    s

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include '<string.h>'
+
 /**
  * puts2 - prints every other character of a string,
  * starting with the first character
@@ -14,10 +14,7 @@ for (i = 0; str[i] != '\0' && str[i + 1] != '\0' ; i += 2)
 {
 _putchar(str[i]);
 }
-if (strlen(str)>20)
-{
-_putchar('.');
-}
+
 _putchar('\n');
 
 }
