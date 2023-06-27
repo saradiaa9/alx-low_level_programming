7415c6be4e66ceb771bd14a34e0350b114e7751c
Sara Diaa <saradiaa9@example.com>
2023-06-26 17:03:29 -0700

    s

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,8 +12,8 @@ void puts2(char *str)
 {
 char *delimiter = strchr(str, '\0');
 *delimiter = '\0';
-char *string1 = str;
-
+char *string1
+*string1=str;
 int i;
 for (i = 0; string1[i] != '\0' && string1[i + 1] != '\0' ; i += 2)
 {
