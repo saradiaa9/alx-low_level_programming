aa931a9de91df53d084019b6c870b813da81f297
Sara Diaa <saradiaa9@example.com>
2023-06-26 17:01:37 -0700

    s

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <stdio.h>
+#include <string.h>
 /**
  * puts2 - prints every other character of a string,
  * starting with the first character
@@ -9,10 +10,14 @@
  */
 void puts2(char *str)
 {
+char *delimiter = strchr(str, '\0');
+*delimiter = '\0';
+char *string1 = str;
+
 int i;
-for (i = 0; str[i] != '\0' && str[i + 1] != '\0' ; i += 2)
+for (i = 0; string1[i] != '\0' && string1[i + 1] != '\0' ; i += 2)
 {
-_putchar(str[i]);
+_putchar(string1[i]);
 }
 
 _putchar('\n');
