d417a5be74911afd5b4d7c45c9b2bff1f5196141
Sara Diaa <saradiaa9@example.com>
2023-06-26 17:13:26 -0700

    s

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,10 +14,11 @@ void puts2(char *str)
 
 int i;
 char *string1;
+
 string1 = str;
 
 
-for (i = 0; string1[i] != '\0' && string1[i + 1] != '\0'; i += 2)
+for (i = 0; string1[i] != '\0'; i += 2)
 {
 _putchar(string1[i]);
 }
