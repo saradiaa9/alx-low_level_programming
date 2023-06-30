008c41237693296e73737afdb57e9989f19a0334
Sara Diaa <saradiaa9@example.com>
2023-06-30 03:24:41 -0700

    s

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,10 @@
  *         0 if s1 is equal to s2,
  *         Positive value if s1 is greater than s2
  */
-int _strcmp(char *s1, char *s2)
-{
-while (*s1 != '\0' && *s2 != '\0')
-{
-if (*s1 != *s2)
-{
-return ((*s1 < *s2) ? -15 : 15);
+int _strcmp(char *s1, char *s2) {
+int i = 0;
+while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0') {
+i++;
 }
-s1++;
-s2++;
-}
-
-return ((*s1 == '\0' && *s2 == '\0') ? 0 : (*s1 == '\0') ? -15 : 15);
+return (s1[i] - s2[i]);
 }
