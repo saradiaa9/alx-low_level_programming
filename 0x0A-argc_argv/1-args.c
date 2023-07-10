24cc1a567068ee70a579cb891e87d4e70c1a3199
saradiaa9 <saradiaa9@gmail.com>
2023-07-10 16:19:59 -0700

    s

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main1 - Entry point
+ * @argc: The number of arguments passed to the program
+ *
+ * Return: Always 0 (success)
+ */
+int main1(int argc)
+{
+printf("%d\n", argc);
+
+return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 int main(int argc, char *argv[]);
+int main1(int argc, char *argv[]);
 
 #endif /* MYLIB_H */
