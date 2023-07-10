7e4c30ccb0c3fa87018302231c539203c9e7cc3c
saradiaa9 <saradiaa9@gmail.com>
2023-07-10 16:33:55 -0700

    s

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,15 +14,16 @@ if (size == 0)
 {
 return (NULL);
 }
-
-char *arr = malloc(sizeof(char) * size);
+char *arr;
+unsigned int i;
+*arr = malloc(sizeof(char) * size);
 
 if (arr == NULL)
 {
 return (NULL);
 }
 
-for (unsigned int i = 0; i < size; i++)
+for ( i = 0; i < size; i++)
 {
 arr[i] = c;
 }
