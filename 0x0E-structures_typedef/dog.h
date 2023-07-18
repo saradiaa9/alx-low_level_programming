ab24fa10192d33b8b20d4ae72443676f40ab807e
saradiaa9 <saradiaa9@gmail.com>
2023-07-18 12:43:55 -0700

    s

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,12 @@
-struct dog {
+/**
+ * struct dog - a struct representing a dog
+ *
+ * @name: pointer to a character array representing the dog's name
+ * @age: floating-point number representing the dog's age
+ * @owner: pointer to a character array representing the dog's owner
+ */
+struct dog
+{
 char *name;
 float age;
 char *owner;
