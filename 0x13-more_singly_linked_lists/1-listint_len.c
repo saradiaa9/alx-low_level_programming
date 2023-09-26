70b5b1922689e8c0c6b96b202ad6077a48b45ae5
saradiaa9 <saradiaa9@gmail.com>
2023-09-26 09:29:32 -0700

    s

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t node_count = 0; /* To keep track of the number of nodes */
+size_t node_count = 0; /* To keep track of the number of nodes */
 
-    while (h != NULL)
-    {
-        node_count++;   /* Increment the node count */
-        h = h->next;    /* Move to the next node */
-    }
+while (h != NULL)
+{
+node_count++;   /* Increment the node count */
+h = h->next;    /* Move to the next node */
+}
 
-    return (node_count);
+return (node_count);
 }
