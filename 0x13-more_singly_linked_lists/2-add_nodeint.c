d969848b2bbf8c5d69ddd10964f5d048a31b6ee1
saradiaa9 <saradiaa9@gmail.com>
2023-09-26 09:30:45 -0700

    s

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,7 +15,7 @@ listint_t *new_node;
 
 new_node = malloc(sizeof(listint_t));
 if (new_node == NULL)
-return NULL; 
+return (NULL); 
 
 
 new_node->n = n;
@@ -26,5 +26,5 @@ new_node->next = *head;
 
 *head = new_node;
 
-return new_node;
+return (new_node);
 }
