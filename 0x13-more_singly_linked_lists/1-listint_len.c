b9b237b619908a603055a185dc5a1d242e2a5bbb
saradiaa9 <saradiaa9@gmail.com>
2023-09-26 09:17:28 -0700

    s

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements in a listint_t list.
+ * @h: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of elements (nodes) in the list.
+ */
+size_t listint_len(const listint_t *h)
+{
+    size_t node_count = 0; /* To keep track of the number of nodes */
+
+    while (h != NULL)
+    {
+        node_count++;   /* Increment the node count */
+        h = h->next;    /* Move to the next node */
+    }
+
+    return (node_count);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,6 +21,7 @@ typedef struct listint_s
 
 int _putchar(char c);  /* Prototype for the _putchar function */
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 /* More declarations specific to your header */
 
